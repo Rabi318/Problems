cdb28013bdf82753e752fb9a14f271c613b33899
RabinarayanS <rabinarayansahoo9348@gamil.com>
2025-02-14 21:49:39 +0530

    problem8 updated

---
+++ Assignments/Problem8.js
@@ -0,0 +1,89 @@
+/*
+#Exercise 1 :
+    Binary Number System : 0, 1
+       base - 2
+    Ocatl Number System : 0, 1, 2, 3, 4, 5, 6, 7
+       base - 8
+    Hexadecimal Number System : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F
+       base - 16
+    ASCII - American Standard Code for Information Interchange
+    ISCII - Indian Script Code for Information Interchange
+
+#Exercise 2 :
+  (514)10 = (1002)8
+  (220)8 = (144)10
+  (76F)16 = (1903)10
+  (4D9)16 = (1241)10
+  (11001010)2 = (202)10
+  (1010111)2 = (87)10
+
+#Exercise 3 :
+  (54)10 = (110110)2
+  (120)10 = (1111000)2
+  (76)10 = (114)8
+  (889)10 = (1571)8
+  (789)10 = (315)16
+  (108)10 = (6C)16
+
+#Exercise 4:
+  Express the following octal numbers into their equivalent decimal numbers
+  145 = 101
+  6760 = 3568
+  455 = 301
+
+#Exercise 5:
+  Decimal to hexadecimal conversion
+  548 = 224
+  4052 = FD4
+  58 = 3A
+
+#Exercise 6
+Hexadecimal to decimal conversion
+  4A2 = 1186
+  9E1A = 40474
+  6BD = 1725
+
+#Exercise 7
+Binary to octal and hexadecimal conversion
+  1110001000 = (1610)8, (388)16
+  110110101 = (665)8, (1B5)16
+  1010100 = (124)8, (54)16
+
+#Exercise 8
+Octal to binary conversion
+
+  2306 = 010 011 000 110
+  5610 = 101 110 001 000
+  742 = 111 100 101
+
+#Exercise 9
+Hexadecimal to binary conversion
+
+  4026 = 0100 0000 0010 0110
+  BCA1 = 1011 1100 1010 0001
+  98E = 1001 1110 1110
+
+#Exercise 10
+Understanding text by computers
+
+  HOTS
+  H = 72 = 1001000
+  O = 79 = 1001111
+  T = 84 = 1010100
+  S = 83 = 1010011
+
+  Main
+  M = 77 = 1001101
+  a = 97 = 1100001
+  i = 105 = 1101001
+  n = 110 = 1101110
+
+  CaSe
+  C = 67 = 1000011
+  a = 97 = 1100001
+  S = 83 = 1010011
+  e = 101 1100101
+
+#Exercise 11
+  16 is the base value of the hexadecimal number system (0-9, A-F).
+*/
