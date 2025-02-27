ec4624a9d61862c74ccd39d59a4998ee84bd29dd
RabinarayanS <rabinarayansahoo9348@gamil.com>
2025-02-27 22:41:03 +0530

    bind use

---
+++ Assignments/module2/sprint2/day2/binduse.js
@@ -0,0 +1,14 @@
+function setTimeoutGreeting(person) {
+  setTimeout(
+    function () {
+      console.log(`Hello, ${this.name}`);
+    }.bind(person),
+    1000
+  );
+}
+
+const person = {
+  name: "Alice",
+};
+
+setTimeoutGreeting(person);
