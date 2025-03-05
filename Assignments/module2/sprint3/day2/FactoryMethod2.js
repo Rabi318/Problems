24cdf2ee30f1be6d5f487c23106c342ecad07442
RabinarayanS <rabinarayansahoo9348@gamil.com>
2025-03-05 21:01:34 +0530

    L0 - Creating and Using Methods in Factory Functions

---
+++ Assignments/module2/sprint3/day2/FactoryMethod2.js
@@ -0,0 +1,13 @@
+function createcar(make, model, year) {
+  return {
+    make,
+    model,
+    year,
+    describeCar() {
+      console.log(`This car is a ${this.year} ${this.make}  ${this.model}.`);
+    },
+  };
+}
+
+const car = createcar("Toyota", "Camry", 2022);
+car.describeCar();
