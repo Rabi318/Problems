2b848e54403602cb0d3f654601da00a900d4166d
RabinarayanS <rabinarayansahoo9348@gamil.com>
2025-06-18 18:41:05 +0530

    system info

---
+++ Assignments/Unit-5/01IntroductiontoBackendNodeJsandModules/system-info/systemInfo.js
@@ -0,0 +1,23 @@
+const os = require("os");
+
+function getSystemInfo() {
+  const cpus = os.cpus();
+  const heap = process.memoryUsage();
+
+  console.log("System Information:");
+  console.log("-------------------------");
+  console.log(`Architecture: ${os.arch()}`);
+  console.log(`CPU Cores: ${cpus.length}`);
+  console.log(`CPU Model: ${cpus[0].model}`);
+  console.log(`CPU Speed: ${cpus[0].speed / 1000} GHz`);
+  console.log(`Total Memory: ${(os.totalmem() / 1024 ** 3).toFixed(2)} GB`);
+  console.log(`Free Memory: ${(os.freemem() / 1024 ** 3).toFixed(2)} GB`);
+  console.log(`Heap Memory Used: ${(heap.heapUsed / 1024 ** 2).toFixed(2)} MB`);
+  console.log(
+    `Heap Memory Total: ${(heap.heapTotal / 1024 ** 2).toFixed(2)} MB`
+  );
+  console.log(`Hostname: ${os.hostname()}`);
+  console.log(`OS Type: ${os.type()}`);
+}
+
+module.exports = getSystemInfo;
